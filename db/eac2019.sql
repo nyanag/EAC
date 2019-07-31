ebcb3c152cef8b1b140f80f35ee52f521cf6cc0e
satyavishal <47266380+satyavishal@users.noreply.github.com>
2019-07-31 10:39:42 +0530

    fixed bugs
    fixed bugs in register.php
    updated the database

--- db/eac2019.sql
+++ db/eac2019.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 29, 2019 at 05:48 PM
+-- Generation Time: Jul 31, 2019 at 05:08 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.14
 
@@ -25,6 +25,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `attendance`
+--
+
+DROP TABLE IF EXISTS `attendance`;
+CREATE TABLE IF NOT EXISTS `attendance` (
+  `reg_id` varchar(9) NOT NULL,
+  `attendance` text NOT NULL,
+  PRIMARY KEY (`reg_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `participants`
 --
 
@@ -33,15 +46,44 @@ CREATE TABLE IF NOT EXISTS `participants` (
   `Reg_id` varchar(9) NOT NULL,
   `Type` text NOT NULL,
   `Name` text NOT NULL,
-  `Phone` int(10) NOT NULL,
+  `Phone` varchar(10) NOT NULL,
   `Email` text,
   `Reg_no` varchar(9) DEFAULT NULL,
   `College` text NOT NULL,
-  `Payment_status` text,
-  `Paid_to` text,
   PRIMARY KEY (`Reg_id`),
   UNIQUE KEY `Reg_no` (`Reg_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+DROP TABLE IF EXISTS `payments`;
+CREATE TABLE IF NOT EXISTS `payments` (
+  `reg_id` varchar(10) NOT NULL,
+  `payments_status` text NOT NULL,
+  `ref_id` text NOT NULL,
+  `paid_to` text NOT NULL,
+  PRIMARY KEY (`reg_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`reg_id`) REFERENCES `participants` (`Reg_id`);
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`reg_id`) REFERENCES `participants` (`Reg_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- register.php
+++ register.php
@@ -15,69 +15,41 @@ $rowscountquery="SELECT * from participants;";
 $noofrows=mysqli_query($conn,$rowscountquery);
 $rowsres=mysqli_num_rows($noofrows);
 echo $rowsres;
-// if(isset($_POST['submit'])){
-// 	if(strlen($_POST['first_name'])>0)
-// 	{
-// 		$Regid='19eac'.($rowsres+1);
-// 		$Name=$_POST['first_name'];
-// 		$Phone=$_POST['phone'];
-// 		$College=$_POST['college'];
-// 		$Email=$_POST['email'];
-// 		$queryforexternals="INSERT INTO participants(Reg_id,Type,Name,Phone,Email,College) values('$Regid','External','$Name','$Phone','$email','$College');";
-// 		if ($conn->query($queryforexternals) === TRUE) {
-// 			echo "New record created successfully";
-// 		} else {
-// 			echo "Error: " . $sql . "<br>" . $conn->error;
-// 		}
-
-// 	}
-// 	if(strlen($_POST['firstName'])>0)
-// 	{
-// 		$Regid='19eac'.($rowsres+1);
-// 		$Name=$_POST['firstName'];
-// 		$Phone=$_POST['phone_'];
-// 		$Regno=$_POST['regNo'];
-// 		$Email=$_POST['email'];
-// 		$queryforexternals="INSERT INTO participants(Reg_id,Type,Name,Phone,Email,Reg_no,College) values('$Regid','Internal','$Name','$Phone','$email','$Regno','VIT Vellore');";
-// 		if ($conn->query($queryforexternals) === TRUE) {
-// 			echo "New record created successfully";
-// 		} else {
-// 			echo "Error: " . $sql . "<br>" . $conn->error;
-// 		}
-
-// 	}
-// }
-
-//asdfasdfasdfa
-
-
 if(isset($_POST['submit'])){
-	$Regid='19eac'.($rowsres+1);
-	$Phone=$_POST['phone'];
-	$email=$_POST['email'];	
 	if(strlen($_POST['first_name'])>0)
 	{
+		$Regid='19eac'.($rowsres+1);
 		$Name=$_POST['first_name'];
+		$Phone=$_POST['phone'];
 		$College=$_POST['college'];
+		$email=$_POST['Email'];
 		$queryforexternals="INSERT INTO participants(Reg_id,Type,Name,Phone,Email,College) values('$Regid','External','$Name','$Phone','$email','$College');";
 		if ($conn->query($queryforexternals) === TRUE) {
-			echo "New record created successfully";
+			// echo "New record created successfully";
+			header("location:register.html");
 		} else {
-			echo "Error: " . "<br>" . $conn->error;
+			// echo "Error: " .$conn. "<br>" . $conn->error;
+			header("location:index.html");
 		}
+
 	}
-	else if(strlen($_POST['firstName'])>0)
+	if(strlen($_POST['firstName'])>0)
 	{
+		$Regid='19eac'.($rowsres+1);
 		$Name=$_POST['firstName'];
+		$Phone=$_POST['phone_'];
 		$Regno=$_POST['regNo'];
+		$email=$_POST['email'];
 		$queryforinternals="INSERT INTO participants(Reg_id,Type,Name,Phone,Email,Reg_no,College) values('$Regid','Internal','$Name','$Phone','$email','$Regno','VIT Vellore');";
 		if ($conn->query($queryforinternals) === TRUE) {
-			echo "New record created successfully";
+			// echo "New record created successfully";
+			header("location:register.html");
 		} else {
-			echo "Error: " . "<br>" . $conn->error;
+			// echo "Error: " .$conn. "<br>" . $conn->error;
+			header("location:index.html");
+		
 		}
 
 	}
 }
-
 	?>
